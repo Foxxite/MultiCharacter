da4680ce8e18da14e57ff1f2df06d9e3d0065f5f
Foxxite <foxxite@thefoxes.eu>
2020-04-26 19:31:06 +0200

    Save data even if player is offline

--- src/main/java/com/foxxite/multicharacter/events/PlayerQuitEventListener.java
+++ src/main/java/com/foxxite/multicharacter/events/PlayerQuitEventListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuitEventListener implements Listener {
@@ -18,6 +19,18 @@ public class PlayerQuitEventListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerQuit(final PlayerQuitEvent event) {
+        final Player player = event.getPlayer();
+
+        if (this.plugin.getActiveCharacters().containsKey(player.getUniqueId())) {
+            final Character character = this.plugin.getActiveCharacters().get(player.getUniqueId());
+            character.saveData(player);
+            this.plugin.getActiveCharacters().remove(player.getUniqueId());
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerKick(final PlayerKickEvent event) {
         final Player player = event.getPlayer();
 
         if (this.plugin.getActiveCharacters().containsKey(player.getUniqueId())) {
--- src/main/java/com/foxxite/multicharacter/tasks/SaveCharacterTask.java
+++ src/main/java/com/foxxite/multicharacter/tasks/SaveCharacterTask.java
@@ -2,7 +2,7 @@ package com.foxxite.multicharacter.tasks;
 
 import com.foxxite.multicharacter.MultiCharacter;
 import com.foxxite.multicharacter.misc.Character;
-import org.bukkit.Bukkit;
+import com.foxxite.multicharacter.misc.Common;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
@@ -19,13 +19,16 @@ public class SaveCharacterTask extends TimerTask {
 
     @Override
     public void run() {
-        for (final Player player : Bukkit.getOnlinePlayers()) {
+        final HashMap<UUID, Character> localActiveCharacters = (HashMap<UUID, Character>) this.plugin.getActiveCharacters().clone();
 
-            final HashMap<UUID, Character> localActiveCharacters = (HashMap<UUID, Character>) this.plugin.getActiveCharacters().clone();
+        localActiveCharacters.forEach((uuid, character) -> {
+            character.saveData();
 
-            if (localActiveCharacters.containsKey(player.getUniqueId())) {
-                localActiveCharacters.get(player.getUniqueId()).saveData();
+            final Player player = Common.getPlayerByUuid(uuid);
+
+            if (!player.isOnline() || player == null) {
+                this.plugin.getActiveCharacters().remove(player.getUniqueId());
             }
-        }
+        });
     }
 }

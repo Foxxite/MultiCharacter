7ab9f8f9d454556c6b872d87d0668e13356f58fd
Foxxite <foxxite@thefoxes.eu>
2020-06-06 19:20:52 +0200

    Added LGBT Steve

--- src/main/java/com/foxxite/multicharacter/worldspacemenu/WorldSpaceMenu.java
+++ src/main/java/com/foxxite/multicharacter/worldspacemenu/WorldSpaceMenu.java
@@ -29,14 +29,14 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class WorldSpaceMenu implements Listener {
 
-    final String textureValue = "ewogICJ0aW1lc3RhbXAiIDogMTU5MTM3MjMyODQwOCwKICAicHJvZmlsZUlkIiA6ICJiZWNkZGIyOGEyYzg0OWI0YTliMDkyMmE1ODA1MTQyMCIsCiAgInByb2ZpbGVOYW1lIiA6ICJTdFR2IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzlhODY3OTM1OWJiN2NiODUzYzUyYmFhMDNhYWZlZTU3MmM4ZTE0NGRjNWI4NTlmNzY1YjI2NTNiMTkxNGM0ODIiCiAgICB9CiAgfQp9";
-    final String textureSignature = "F3zJ4gOCyWCsCjP3JyiY0/Il+GqmJDyc1OGNiBzRny4RhKn+8JGoAyGfcp77UL3x9JzTcn/V37b3qIWcyPKOAxP391QKtHgKmGthi6vc+rIdRZNMlBU0rSVZWizb9nG9Qmjhjl1APCJa26T2g7Wt7yePCLQV5feBlBkv8GRt+GKSmPtiuTFRXzY/fkFemHwxlyGJqoVMcoyW/xmXeV2pq2ZDLSaLH8UwiucPQcc5uv87fSmrRvScdm7auzteXQgcPJBx7zost5/Q0IK+g0q033pzwbA5uU4Qp3tfPesrMKIC2PtbeuKyu+IXaj0SVOZjO5KSZKRiSvtVdiyoc8jN3YxZl5u0Dln0LEuHHBUvIxOjz69fq7syHJTQ++8fHf+7Gfn2GEKQRELIDbQ6MrPwq/N+Y0RbO+nlzWSt0TMVSR27/bqu2VaMgHLnpc0pFN8aPHb5sGotYhzzyRDG4joRCDntYc3ZfaoUi8DkPQy5c5zXqakmb/riPCsKYT3rxKpzPdAKS6fU3ulg6WIJVSaeKlIjIcXjam7NhP9l+ze0W71MFuQJTJYeqYimrCO8zGqN9M3/Yr9Ua20GCfJ2IcYd0NvB/FF7+jW7qZkkj190M0ZmkfURyXZc2UyUNcrstCaK5Ykg1SP/ZIkaiq+lcS+lNxuEowmSfUlNnib0xzGHHzU=";
     private final MultiCharacter plugin;
     private final MinecraftServer server;
     private final CraftPlayer cPlayer;
@@ -49,6 +49,8 @@ public class WorldSpaceMenu implements Listener {
     private final FileConfiguration config;
     private final NamespacedKey namespacedKey;
     private final Location playerLoginLocation;
+    private String textureValue;
+    private String textureSignature;
     private Location lastArmorStandPos;
     private ArrayList<ArmorStand> charInfoStands = new ArrayList<>();
     private int lastSelectedStand = -1;
@@ -61,6 +63,9 @@ public class WorldSpaceMenu implements Listener {
     private ArmorStand staffModeStand;
 
     public WorldSpaceMenu(MultiCharacter plugin, Player player) {
+
+        skinEasterEgg();
+
         this.plugin = plugin;
         this.player = player;
 
@@ -115,6 +120,21 @@ public class WorldSpaceMenu implements Listener {
             }
         }.runTaskLater(plugin, 10L);
 
+    }
+
+    private void skinEasterEgg() {
+
+        LocalDate currentDate = LocalDate.now();
+
+        if (currentDate.getMonth() == Month.JUNE) {
+            // LGBT Steve
+            textureValue = "ewogICJ0aW1lc3RhbXAiIDogMTU5MTQ2MzY3NzExNSwKICAicHJvZmlsZUlkIiA6ICI3MmNiMDYyMWU1MTA0MDdjOWRlMDA1OTRmNjAxNTIyZCIsCiAgInByb2ZpbGVOYW1lIiA6ICJNb3M5OTAiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzA2OWZjMDczOWEwNTFmMzZiZmM5YmM5ZDk1ZGI2NDZiMGE5NWZjZGEzZDViN2UyNmNkY2FhODg1NjAxOGYxOSIKICAgIH0KICB9Cn0=";
+            textureSignature = "ouKYkQjA83oV7A5tPi9G6uCN5X5cC9cxgtAXnZ+OJnPovg7S0FY4OPhJoQEziIX7s5QigMCk1ccTujBODMof83PKWofaS0SjmTQpHN2fVzd30qj/sWOvMYhub43VqGK+o9hZlXAZ4rg8KOIlJLdFkRRyzng4LTd4QdsmNsSUQl+CTYsJ+8riA0iZ88kMzD5bGKOSQZWvgbr3ScKwYhKkD77cvODmyw+KmGT+ssJjSxOke8AnooLLupJ55lczFHmpxGElWTdxvRmHfh9P4NN04hYHTTucVxztbgM4xq3BDBIxOn/0PaVOZnNNectqVTwe89xLCoKAwkJ4QACOpBycD4mCAGVIXEkr7ZXytj5PaeodLdkU1hQ3Loyui4WZx7vbnl8nQG7o4ZB8iq44R2CkyaLNUL/jn7mvY2Dxz6yrJ5oRR1FKM3ucNTWKWpe8xaFe+zUBWh3HLt80pVxH6wt96wPAcD0TiWjz0CHyUbI8P6bJr4fKIZMrH7SubjVy4g/h0gjnr4OnRshWzpI1/Sw6LaqwUmJIZdPXEVij1rI0Odd7VB+i29jLEpFbUki7jnBOxG1lgSv8bloeKizug4JYuUhMifZ4q7Wr+hlWttBxlOMewMzHwOPSg3LJxsMJfJ1jcTczpkVAbJXIoxnUVqXqlsNyc20Bfad20A5Oe0qZ7nI=";
+        } else {
+            // Green Steve Data
+            textureValue = "ewogICJ0aW1lc3RhbXAiIDogMTU5MTQ2MzQ2MTEzMSwKICAicHJvZmlsZUlkIiA6ICI3MzgyZGRmYmU0ODU0NTVjODI1ZjkwMGY4OGZkMzJmOCIsCiAgInByb2ZpbGVOYW1lIiA6ICJ4cWwiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNzUyN2QwZDE5NGE4NDNlZmE2NTkyNDI2NmE5ODAzM2RiYjM0MDYzNjAzMGExN2EyM2JiNTljNzNjNjkyNGVmOCIKICAgIH0KICB9Cn0=";
+            textureSignature = "EcK2+2xLJLCAfu8qvTqtpfQieFh/r43xkDm2a/c2ZIVxvutgxUIZcFp+cjBEttPESDIoc9FeFAgwGvdPi8cgearoqQE9FPzQo/aZdSvwJ1U6hPzdUgt0puZDMFxEWpdUl5rFuL/NMmdrAOOFzANlVlxWZKjnc32xCfL/NdHt2RUku9jRb+v1WISO1rmUng8WsUfte7JqCphVrl6YSySI3669NS0bbqMsSQMCYbzopV4wanYPnN3S3mtOt76GioysME//+TJlER9xuSTuV2+vfxL8L8UbcHiRavOLU6Wd1hdTFaJX2c6NHatffOXZtxIyxkVycyTFDsjV/ssx3jQFUqgPk+z2zvio5UlnWSQtAV2t8+E87uTJEestoslEVniGLfHGF22o4kH1f66dXUG94qMZk7xZkHQqDfK/IC1CNh4w/JV8yKsqRGEmg7f8dNXsMSEcqvsJKXg971loTfO32EDDvHXzjSKIktFnNv6WVH44VcoNQDB8tzetviBxXihQKXdW6GC9bmFoXjqGPrizIYtOL4QhsYqDNSY0wHMsi5JpxNBZXH58fQ7W7RPQg4RupuUw2oxmuRjYQGSX7kpHFY39ZCgTgGWmtg3Ddo94mSNsNvdQXo00eN/YyhRLswO0DlF0NayeJebh2kG7BLwez+O/9JzufjPOziuukZhocSY=";
+        }
     }
 
     public void closeMenu(boolean showPlayer) {
@@ -525,7 +545,6 @@ public class WorldSpaceMenu implements Listener {
             localStand.setCustomName(pointerLeft + language.getMessage("character-selection.new-character.name") + pointerRight);
         }
 
-
         EntityArmorStand entityArmorStand = ((CraftArmorStand) localStand).getHandle();
         PacketPlayOutEntityMetadata metadata = new PacketPlayOutEntityMetadata(entityArmorStand.getId(), entityArmorStand.getDataWatcher(), true);
         connection.sendPacket(metadata);
@@ -576,7 +595,6 @@ public class WorldSpaceMenu implements Listener {
         }
 
         // Update NPC skin
-
         PropertyMap pm = profile.getProperties();
 
         Property property = pm.get("textures").iterator().next();
